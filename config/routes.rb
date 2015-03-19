634055d551a678bec0e06f3345022245c56c3fb4
Andres De Leon <agldeleon@gmail.com>
2015-03-19 09:58:35 -0600

    Create Results controller with index action and make it the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'results/index' => 'results#index'
+
+  root 'results#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
