4349a542324ab123916d117169bfa15d2ea67bcc
Andres De Leon <agldeleon@gmail.com>
2015-04-13 11:18:35 -0500

    Implement institution filter with results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,8 +5,11 @@ class ResultsController < ApplicationController
   end
 
   def search
-  	if params[:salary].strip.length > 0
-  		@var = params[:salary]
+  	@institutions = Institution.all
+  	if params[:institution].strip.length > 0
+  		@jobs = Job.where(available: true, institution_id: params[:institution]).order(:institution_id).page params[:page]
+  	else
+  		@jobs = Job.where(available: true).order(:institution_id).page params[:page]
   	end
   end
 end
